5e14e52e42385d3f63bacaac7b12bd61ac45ad60
Ahmad Zaki <166486850+ahmadzakiyox@users.noreply.github.com>
2024-08-21 15:05:15 +0700

    update routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -460,7 +460,7 @@ router.get('/download/savefrom', async (req, res, next) => {
              res.json({
                  status: true,
                  creator: `${creator}`,
-                 url: vid.url
+                 url: vid.url,
                  thumb: vid.thumb
              })
          })
