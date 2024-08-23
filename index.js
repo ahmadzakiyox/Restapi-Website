379daef51e4a7d74330908dc4e8a4c6a11b53ae1
Ahmad Zaki <166486850+ahmadzakiyox@users.noreply.github.com>
2024-08-24 00:03:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ var { color } = require('./lib/color.js')
 var mainrouter = require('./routes/main'),
     apirouter = require('./routes/api')
 
+const fs = require('fs');
+const path = require('path');
+const countFile = path.join(__dirname, 'count.txt');
 var app = express()
 app.enable('trust proxy');
 app.set("json spaces",2)
@@ -14,6 +17,25 @@ app.use(cors())
 app.use(secure)
 app.use(express.static("public"))
 
+app.get('/count', (req, res) => {
+    fs.readFile(countFile, 'utf8', (err, data) => {
+      if (err) {
+        return res.status(500).send('Error reading count file.');
+      }
+  
+      let count = parseInt(data, 10);
+      if (isNaN(count)) count = 0;
+  
+      count += 1;
+  
+      fs.writeFile(countFile, count.toString(), (err) => {
+        if (err) {
+          return res.status(500).send('Error updating count file.');
+        }
+        res.json({ count });
+      });
+    });
+  });
 app.use('/', mainrouter)
 app.use('/api', apirouter)
 
